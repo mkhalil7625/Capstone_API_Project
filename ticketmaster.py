66dbdb096137734c3f7cc9e33168f31468fb2382
Jake-Fischer <jakefischer000@gmail.com>
2021-10-20 14:01:27 -0500

    Create get_events function

--- ticketmaster.py
+++ ticketmaster.py
@@ -1,45 +1,60 @@
-"""
-This is some test code for the ticketmaster api
-"""
-
 import requests
+from ticketmaster_config import api_key, artist
 
-base_url = 'https://app.ticketmaster.com'
-base_path = '/discovery/v2/events.json?'
-key = '&apikey='
-artist = ''
-
-url_string = base_url + base_path + 'keyword=' + artist + key
-
-result = requests.get(url_string)
-
-number_of_pages = result.json()['page']['totalPages']
-number_of_elements = result.json()['page']['totalElements']
-number_of_events = 0
-next_page_path = result.json()['_links']['next']
-
-print(number_of_elements)
-print(number_of_pages)
-print(next_page_path)
-
-def get_json_data(url): # Returns Json data from a request
-    result = requests.get(url)
-    return result.json()
 
 def get_events(artist):
-    base_url = 'https://app.ticketmaster.com'
-    base_path = '/discovery/v2/events.json?'
-    key = '&apikey='
-    events = []
-    new_url = base_url + base_path + 'keyword=' + artist + key
+    response = request_events(artist)
+    json_data = get_json_from_response(response)
+    if check_for_events(json_data):
+        event_list = get_event_list_from_json_data(json_data)
+        print(event_list)
+        return event_list
+    else:
+        print('Sorry, there are no events currently scheduled for this artist in the USA')
+        return None
 
-    first_page = get_json_data(new_url)
-    for i in first_page['_embeded']['events']:
-        events.append(i)
 
-    next_page_path = first_page['_links']['next']['href']
+def request_events(artist):
 
-    if next_page_path:
-        # get_json_data(base_url + next_page_path)
-        pass
+    """
+    Requests a response from ticketmaster, using the artist given as a keyword, and an api_key from a configuration file.
+    """
+    url = f'https://app.ticketmaster.com/discovery/v2/events.json?keyword={artist}&countryCode=US&apikey={api_key}'
+    response = requests.get(url)
+    return response
 
+
+def get_json_from_response(result):
+    return result.json()
+
+
+def get_event_list_from_json_data(data):
+
+    """
+    Parses through the json data to pull our required data, and append it to a list.
+    """
+    event_list = []
+    raw_event_data = data['_embedded']['events']
+
+    for i in raw_event_data:
+        event_dictionary = {}
+        event_dictionary['event_date'] = i['dates']['start']['localDate']
+        event_dictionary['event_time'] = i['dates']['start']['localTime']
+        event_dictionary['venue_name'] = i['_embedded']['venues'][0]['name']
+        event_dictionary['event_location'] = i['_embedded']['venues'][0]['city']['name'] + ', ' + i['_embedded']['venues'][0]['state']['name']
+        event_dictionary['link_for_tickets'] = i['url']
+        event_list.append(event_dictionary)
+    return event_list
+
+
+def check_for_events(data):
+
+    """
+    Check to see if there are any events in the response.
+    """
+    if '_embedded' in data:
+        return True
+    else:
+        return False
+
+needed_event_data = get_events(artist)
