fb4b0d2fdcd1da534340780ff0c04bd00d21d2c8
Jake-Fischer <jakefischer000@gmail.com>
2021-10-21 14:44:31 -0500

    Create model classes with peewee

---
+++ database/model.py
@@ -0,0 +1,41 @@
+from peewee import *
+from database_config import database_path
+
+
+db = SqliteDatabase(database_path)
+
+
+class Bookmarks(Model):
+    artist_name = CharField()
+    image_url = CharField()
+    song1 = CharField()
+    song2 = CharField()
+    song3 = CharField()
+    song4 = CharField()
+    song5 = CharField()
+    spotify_link = CharField()
+    events = CharField()
+
+    class Meta:
+        database = db
+
+    def __str__(self):
+        return f'{self.artist_name}, {self.image_url}, {self.song1}, {self.song2}, {self.song3}, {self.song4}, {self.song5}, {self.spotify_link}, {self.events}'
+
+
+class Cache(Model):
+    artist_name = CharField()
+    image_url = CharField()
+    song1 = CharField()
+    song2 = CharField()
+    song3 = CharField()
+    song4 = CharField()
+    song5 = CharField()
+    spotify_link = CharField()
+    events = CharField()
+
+    class Meta:
+        database = db
+
+    def __str__(self):
+        return f'{self.artist_name}, {self.image_url}, {self.song1}, {self.song2}, {self.song3}, {self.song4}, {self.song5}, {self.spotify_link}, {self.events}'
