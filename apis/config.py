92379e9ad108f19243013f5bd4fdf66d211caf40
Jake-Fischer <jakefischer000@gmail.com>
2021-10-21 14:38:52 -0500

    Change filename to database_config.py'

---
+++ apis/config.py
@@ -0,0 +1,12 @@
+# Ticketmaster
+TICKETMASTER_API_KEY = 'yRwGqef8uYVVnqglVmyETeuZqZErkVR4'
+
+#Spotify
+SPOTIFY_API_KEY = 'a85a3dc276d1485fbfe83dd084c39546'
+SPOTIFY_CLIENT_SECRET = '23be5a6d2e8240749973a28b5e80c1b9'
+
+#Imgur
+IMGUR_API_KEY = 'd3a6d06f9224c86 '
+
+#Self reference
+self = 'config.py'
