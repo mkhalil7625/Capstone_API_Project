439851d91f71323dfcbda64bec09a22548f1d197
Jake-Fischer <jakefischer000@gmail.com>
2021-10-29 13:43:07 -0500

    Create bookmark_data()

--- database/bookmark.py
+++ database/bookmark.py
@@ -9,3 +9,20 @@ def combine_dictionaries(imgur_dictionary, spotify_dictuonary, ticketmaster_dict
     new_dictionary['spotify'] = spotify_dictuonary
     new_dictionary['ticketmaster'] = ticketmaster_dictionary
     return new_dictionary
+
+def bookmark_data(artist_data):
+    db = SqliteDatabase(database_path) # Get the path and connect to db
+    db.connect()
+    db.create_tables([Bookmarks])
+    new_bookmark = Bookmarks.create(
+    artist_name = artist_data['spotify']['artist_name'],
+    image_url = artist_data['imgur']['image_url'],
+    song1 = artist_data['spotify']['top_five_songs'][0],
+    song2 = artist_data['spotify']['top_five_songs'][1],
+    song3 = artist_data['spotify']['top_five_songs'][2],
+    song4 = artist_data['spotify']['top_five_songs'][3],
+    song5 = artist_data['spotify']['top_five_songs'][4],
+    spotify_link = artist_data['spotify']['spotify_page_url'],
+    events = json.dumps(artist_data['ticketmaster']['events'])
+    )
+    new_bookmark.save()
