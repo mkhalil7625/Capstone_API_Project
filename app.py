9047fb49c8621b5de8f9df8b5d274a10a46e0064
Fernando Molano <fm87usa@gmail.com>
2021-10-30 22:46:54 -0400

    update styling and html structure to load right image

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask,request, render_template, redirect
 # todo import database
-from apis import ticketmaster,spotify
+from apis import ticketmaster,spotify, ImgurAPI
+import os
+
 
 app = Flask(__name__)
 
@@ -13,7 +15,25 @@ def get_artist_info():
     artist_name = request.args.get('search')
     artist_spotify_info = spotify.get_artist_music(artist_name)
     artist_events = ticketmaster.get_events(artist_spotify_info['artist_name'])
-    # artist_url=
+    artist_url = ImgurAPI.getpicture(artist_spotify_info['artist_name'])
 
-    return render_template('artist.html',events=artist_events, artist_info=artist_spotify_info)
+    return render_template('artist.html',events=artist_events, artist_info=artist_spotify_info, artist_image=artist_url)
 
+@app.route('/save-artist')
+def save_artist():
+    # Ask database to save artist
+    # bookmark.bookmark_data(artist_data)
+
+    print(request.data.get('artist_name'))
+    return redirect('/display-all-bookmarks')
+
+
+# @app.route('/display-all-bookmarks')
+#     #List of all
+
+# @app.route('/error')
+#     #Error handling
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
